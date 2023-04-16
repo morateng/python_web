72b1a12e4d21cd8770e3d33060576bdf325aed80
KangJaeHoon <morateng@naver.com>
2023-04-17 00:57:47 +0900

    made image generative web by openai

--- my_app_jaehoon_pynecone/my_app_jaehoon_pynecone.py
+++ my_app_jaehoon_pynecone/my_app_jaehoon_pynecone.py
@@ -1,41 +1,66 @@
-"""Welcome to Pynecone! This file outlines the steps to create a basic app."""
-from pcconfig import config
-
 import pynecone as pc
+import openai
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
+openai.api_key = "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"
 
 
 class State(pc.State):
     """The app state."""
+    prompt = ""
+    image_url = ""
+    image_processing = False
+    image_made = False
 
-    pass
+    def process_image(self):
+        """Set the image processing flag to true and indicate image is not made yet."""
+        self.image_processing = True
+        self.image_made = False
+
+    def get_image(self):
+        """Get the image from the prompt."""
+        response = openai.Image.create(
+            prompt=self.prompt, n=1, size="1024x1024")
+        self.image_url = response["data"][0]["url"]
+        self.image_processing = False
+        self.image_made = True
 
 
-def index() -> pc.Component:
+def index():
     return pc.center(
         pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
+            pc.heading("텍스트를 이미지로 변환해드립니다(made by jaehoon)",
+                       font_size="1.5em"),
+            pc.input(placeholder="Enter a prompt..", on_blur=State.set_prompt),
+            pc.button(
+                "Generate Image",
+                on_click=[State.process_image, State.get_image],
+                width="100%",
             ),
-            spacing="1.5em",
-            font_size="2em",
+            pc.divider(),
+            pc.cond(
+                State.image_processing,
+                pc.circular_progress(is_indeterminate=True),
+                pc.cond(
+                    State.image_made,
+                    pc.image(
+                        src=State.image_url,
+                        height="25em",
+                        width="25em",
+                    )
+                )
+            ),
+            bg="white",
+            padding="2em",
+            shadow="lg",
+            border_radius="lg",
         ),
-        padding_top="10%",
+        width="100%",
+        height="100vh",
+        bg="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%)",
     )
 
 
 # Add state and page to the app.
 app = pc.App(state=State)
-app.add_page(index)
+app.add_page(index, title="Pynecone:DALL·E")
 app.compile()
